7ad21c317a35577c2713f0a8c6a7dabd85967382
lutherjohn <lutherjohn.1987@gmail.com>
2022-05-05 16:03:19 +0800

    [feature-001] adding cards and pagination

---
+++ src/components/ProfileCards.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <b-card
+        v-for="data in dataList"
+        :bg-variant="data.variant"
+        text-variant="white"
+        :header="data.name"
+        class="text-center mx-3 mb-3"
+        :key="data"
+      >
+        <b-card-text>{{ data.description }}</b-card-text>
+      </b-card>
+    </div>
+    <template v-if="dataList.length > 4">
+      <div>
+        <p>pagination</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileCards",
+  data: () => ({
+    dataList: [
+      {
+        id: 1,
+        name: "Pitok",
+        description: "Full Stack Developer",
+        variant: "primary",
+      },
+      {
+        id: 2,
+        name: "Joan",
+        description: "Quality Assurance",
+        variant: "secondary",
+      },
+      {
+        id: 3,
+        name: "Lexie",
+        description: "Software Engineer",
+        variant: "success",
+      },
+      {
+        id: 4,
+        name: "Chester",
+        description: "Frontend Developer",
+        variant: "dark",
+      },
+      {
+        id: 5,
+        name: "Justin",
+        description: "Frontend Developer",
+        variant: "secondary",
+      },
+      {
+        id: 6,
+        name: "Joshua",
+        description: "Frontend Developer",
+        variant: "primary",
+      },
+    ],
+  }),
+};
+</script>
